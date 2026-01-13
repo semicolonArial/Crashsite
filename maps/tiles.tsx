09627993110b6fcec0fed4ab7c259767763c6825
semicolonArial <phillipaadlanddale@gmail.com>
2026-01-13 20:45:31 +0000

    Delete maps/tiles.tsx

--- maps/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tiles" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="tiles.ase" width="256" height="256"/>
-</tileset>
